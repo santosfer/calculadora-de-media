5a13a156c280a70c99251da1f988c8fc0a293186
Fernanda Santos <santo4fer@gmail.com>
2023-01-30 12:15:57 -0300

    changing msg style

--- calculadoraDeMedia.js
+++ calculadoraDeMedia.js
@@ -10,8 +10,14 @@ function Converter(){
     if(valorMediaFinal >= 7){
     valorMediaFinalTex.innerHTML = " Sua média é " + valorMediaFinal + " você foi aprovado!"
       valorMediaFinalTex.style.color = '#006400'
+      valorMediaFinalTex.style.backgroundColor = '#98FB98'
+      valorMediaFinalTex.style.padding = '10px'
+      valorMediaFinalTex.style.width = '400px'
   } else {
     valorMediaFinalTex.innerHTML = " Sua média é " + valorMediaFinal + " você reprovou"
     valorMediaFinalTex.style.color = '#FF0000'
+    valorMediaFinalTex.style.backgroundColor = '#FFA07A'
+    valorMediaFinalTex.style.padding = '10px'
+    valorMediaFinalTex.style.width = '400px'
   }
 }
